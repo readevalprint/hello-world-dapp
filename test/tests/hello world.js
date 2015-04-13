a947d1c14d107959aa098c575bbf1f9773f7848b
David Braun <David.Braun@Toptal.com>
2015-04-12 22:53:40 -0400

    Pass the first 4 Nightwatch tests.

--- test/tests/hello world.js
+++ test/tests/hello world.js
@@ -1,39 +1,51 @@
+var
+  name;
+
+// Choose a non-deterministic name because the keys persist in IPFS
+// between tests.
+name = String(Math.random());
+
 module.exports = {
   "load page": function (browser) {
     browser
       .useXpath()
       .url('http://helloworldone:3000/')
-      .waitForElementVisible('//input[@id="filenameGet"]', 1000);
+      .waitForElementVisible('//input[@id="filenameGet"]', 10000);
   },
 
   "missing file": function (browser) {
     browser
       .setValue('//*[@id="filenameGet"]', "centralized")
       .click('(//button)[2]')
-      .waitForElementVisible('//*[contains(@class, "alertify-log-error")]',
-        1000)
+      .waitForElementVisible('//*[contains(@class, "ajs-error")]', 10000)
 
-      .assert.containsText('//*[contains(@class, "alertify-log-error")]',
+      .assert.containsText('//*[contains(@class, "ajs-error")]',
         "File not found");
   },
 
   "add file to one": function (browser) {
     browser
-      .setValue('//*[@id="filenameAdd"]', "marmots")
+      .setValue('//*[@id="filenameAdd"]', name)
       .setValue('//*[@id="input"]', "http://www.marmotburrow.ucla.edu/index.html")
       .click('(//button)[1]')
-      .waitForElementVisible('//*[contains(@class, "alertify-log-success")]',
-        1000)
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]', 30000)
 
-      .assert.containsText('//*[contains(@class, "alertify-log-success")]',
-        "File sent! You can now get it by its name.");
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "File sent! You can now get it by its name.")
+
+      .waitForElementNotPresent('//*[contains(@class, "ajs-success")]', 10000);
   },
 
   "get file from one": function (browser) {
     browser
       .clearValue('//*[@id="filenameGet"]')
-      .setValue('//*[@id="filenameGet"]', "marmots")
+      .setValue('//*[@id="filenameGet"]', name)
       .click('(//button)[2]')
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]', 30000)
+
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "Got file.")
+
       .assert.value('//*[@id="output"]', "http://www.marmotburrow.ucla.edu/index.html");
   },
 
@@ -42,21 +54,28 @@ module.exports = {
       .url('http://helloworldtwo:3000/')
       .waitForElementVisible('//input[@id="filenameGet"]', 1000)
       .clearValue('//*[@id="filenameGet"]')
-      .setValue('//*[@id="filenameGet"]', "marmots")
+      .setValue('//*[@id="filenameGet"]', name)
       .click('(//button)[2]')
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]', 60000)
+
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "Got file.")
+
       .assert.value('//*[@id="output"]', "http://www.marmotburrow.ucla.edu/index.html");
   },
 
   "change file on two": function (browser) {
     browser
-      .setValue('//*[@id="filenameAdd"]', "marmots")
+      .setValue('//*[@id="filenameAdd"]', name)
       .setValue('//*[@id="input"]', "https://erisindustries.com/")
       .click('(//button)[1]')
-      .waitForElementVisible('//*[contains(@class, "alertify-log-success")]',
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]',
         1000)
 
-      .assert.containsText('//*[contains(@class, "alertify-log-success")]',
-        "File sent! You can now get it by its name.");
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "File sent! You can now get it by its name.")
+
+      .waitForElementNotPresent('//*[contains(@class, "ajs-success")]', 10000);
   },
 
   "get changed file from two": function (browser) {
@@ -64,16 +83,26 @@ module.exports = {
       .url('http://helloworldtwo:3000/')
       .waitForElementVisible('//input[@id="filenameGet"]', 1000)
       .clearValue('//*[@id="filenameGet"]')
-      .setValue('//*[@id="filenameGet"]', "marmots")
+      .setValue('//*[@id="filenameGet"]', name)
       .click('(//button)[2]')
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]', 30000)
+
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "Got file.")
+
       .assert.value('//*[@id="output"]', "https://erisindustries.com/");
   },
 
   "get changed file from one": function (browser) {
     browser
       .clearValue('//*[@id="filenameGet"]')
-      .setValue('//*[@id="filenameGet"]', "marmots")
+      .setValue('//*[@id="filenameGet"]', name)
       .click('(//button)[2]')
+      .waitForElementVisible('//*[contains(@class, "ajs-success")]', 30000)
+
+      .assert.containsText('//*[contains(@class, "ajs-success")]',
+        "Got file.")
+
       .assert.value('//*[@id="output"]', "https://erisindustries.com/");
   },
 
